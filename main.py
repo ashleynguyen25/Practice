6a385c9fd04a11b26d427b871f991f7b70132066
Ashley Nguyen <550112@my.cuhsd.org>
2020-10-15 19:03:56 +0000

    I created functions for the flower petals and the stem so it would be easier to read, and I also created the code that made the stem.

--- main.py
+++ main.py
@@ -4,9 +4,21 @@ trtl.speed(0)
 
 
 #Makes a 18 circles to create a larger circle
-for i in range(18):
+def flower_petals():
+  for i in range(18):
+    trtl.penup()
+    trtl.forward(10)
+    trtl.right(20)
+    trtl.pendown()
+    trtl.circle(20)
+
+#Stem of the flower
+def stem_of_flower():
   trtl.penup()
-  trtl.forward(10)
-  trtl.right(20)
+  trtl.goto(10,-92)
+  trtl.right(90)
   trtl.pendown()
-  trtl.circle(20)
+  trtl.forward(100)
+
+flower_petals()
+stem_of_flower()
